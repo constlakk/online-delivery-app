c26f9edec53f97080a1ed7d809fb0cd823def6a2
Kostas Lakkas <const.lakk@gmail.com>
2023-03-05 03:06:25 +0200

    Database config

--- app.js
+++ app.js
@@ -13,8 +13,8 @@ const fetch = require('node-fetch');
 const Category = require('./models/category');
 const Product = require('./models/product');
 
-mongoose.connect(config.db_connection_string)
-//mongoose.connect(process.env.MONGO_URI)
+//mongoose.connect(config.db_connection_string)
+mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('Connected to database.'))
     .catch(() => console.log('Cannot connect to database'));
 
